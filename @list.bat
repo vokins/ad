f4e5beb3c8078cc144d86739baac14c4dd62e746
vokins <7933104+vokins@users.noreply.github.com>
2023-11-14 22:34:19 +0800

    Update @list.bat
    loon

--- @list.bat
+++ @list.bat
@@ -1,16 +1,16 @@
 @echo off
-del /f qx.list >nul
-copy /y ad.list qx.list
-sed -i "s/DOMAIN,/host, /g" qx.list
-sed -i "s/DOMAIN-SUFFIX,/host-suffix, /g" qx.list
-sed -i "s/DOMAIN-KEYWORD,/host-keyword, /g" qx.list
-sed -i "s/,REJECT/, reject/g" qx.list
+del /f loon.list >nul
+copy /y ad.list loon.list
+sed -i "s/HOST, /DOMAIN,/g" loon.list
+sed -i "s/HOST-KEYWORD, /DOMAIN-KEYWORD,/g" loon.list
+sed -i "s/HOST-SUFFIX, /DOMAIN-SUFFIX,/g" loon.list
+sed -i "s/, REJECT/,REJECT/g" loon.list
 rem 删除前x行注释内容
-sed -i "1,6d" qx.list
+sed -i "1,6d" loon.list
 rem 删除空行
-sed -i "/^$/d" qx.list
+sed -i "/^$/d" loon.list
 rem 删除所有#注释行
-sed -i "/^#/d" qx.list
+sed -i "/^#/d" loon.list
 echo OK
 ping -n 3 127.0.0.1>nul
 exit
