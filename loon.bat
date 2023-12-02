64890b5d426b788899b062f6a36536677f6a8d63
vokins <7933104+vokins@users.noreply.github.com>
2023-12-02 22:10:19 +0800

    Update loon.bat

--- loon.bat
+++ loon.bat
@@ -1,10 +1,10 @@
 @echo off
 del /f loon.list >nul
 copy /y ad.list loon.list
-sed -i "s/HOST, /DOMAIN,/g" loon.list
-sed -i "s/HOST-KEYWORD, /DOMAIN-KEYWORD,/g" loon.list
-sed -i "s/HOST-SUFFIX, /DOMAIN-SUFFIX,/g" loon.list
-sed -i "s/, REJECT/,REJECT/g" loon.list
+sed -i "s/host, /DOMAIN,/g" loon.list
+sed -i "s/host-keyword, /DOMAIN-KEYWORD,/g" loon.list
+sed -i "s/host-suffix, /DOMAIN-SUFFIX,/g" loon.list
+sed -i "s/, reject/,REJECT/g" loon.list
 rem 删除前x行注释内容
 sed -i "1,6d" loon.list
 rem 删除空行
