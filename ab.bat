7f781d10d35b30185bc107c4075259a3ea3737d6
vokins <7933104+vokins@users.noreply.github.com>
2023-12-13 22:49:35 +0800

    Update ab.bat

--- ab.bat
+++ ab.bat
@@ -1,4 +1,31 @@
 @echo off
+del /f adls.list >nul
+copy /y ad.list adls.list
+sed -i "s/host, /DOMAIN,/g" adls.list
+sed -i "s/host-keyword, /DOMAIN-KEYWORD,/g" adls.list
+sed -i "s/host-suffix, /DOMAIN-SUFFIX,/g" adls.list
+sed -i "s/, reject/,REJECT/g" adls.list
+rem 删除前x行注释内容
+sed -i "1,6d" adls.list
+rem 删除空行
+sed -i "/^$/d" adls.list
+rem 删除所有#注释行
+sed -i "/^#/d" adls.list
+echo OK
+ping -n 3 127.0.0.1>nul
+
+del /f prls.list >nul
+copy /y pr.list prls.list
+sed -i "s/host, /DOMAIN,/g" prls.list
+sed -i "s/host-keyword, /DOMAIN-KEYWORD,/g" prls.list
+sed -i "s/host-suffix, /DOMAIN-SUFFIX,/g" prls.list
+sed -i "s/User-Agent, /USER-AGENT,/g" prls.list
+sed -i "s/, proxy/,PROXY/g" prls.list
+rem 删除空行
+sed -i "/^$/d" prls.list
+echo OK
+ping -n 3 127.0.0.1>nul
+
 del /f b.txt ab.txt >nul
 copy /y ad.list b.txt
 sed -i "s/host, /||/g" b.txt
